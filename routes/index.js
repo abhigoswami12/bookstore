f25ed2a6a1e493a8ad349af6d31e2ed7163959a0
abhigoswami12 <abhishekgoswami157@gmail.com>
2020-08-30 10:35:15 +0530

    sorting done but not the most optimized way

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,49 @@ var passport = require('passport');
 var Book = require('../models/Book');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("REQUESTED USER", req.session);
-  Book.find({}, (err, books) => {
-    if(err) next(err);
-    console.log(books)
-    res.render('index', { books });
-  })
+// router.get('/', function(req, res, next) {
+//   console.log("REQUESTED USER", req.session);
+//   Book.find({}, (err, books) => {
+//     if(err) next(err);
+//     console.log(books)
+//     res.render('index', { books });
+//   })
+// });
+
+//read all books on home page
+router.get('/', async function (req, res, next) {
+  try {
+    var books = await Book.find({});
+    res.render("index", { books });
+    
+  } catch (error) {
+      next(error);
+  }
+});
+
+router.get('/price-low-to-high', async (req, res, next) => {
+  try {
+    var books = await Book.find({}).sort({ price: 1 })
+    res.render("index", { books });
+  } catch (error) {
+      next(error);
+  }
+});
+router.get('/price-high-to-low', async (req, res, next) => {
+  try {
+    var books = await Book.find({}).sort({ price: -1 })
+    res.render("index", { books });
+  } catch (error) {
+    next(error);
+  }
+})
+router.get("/newest", async (req, res, next) => {
+  try {
+    var books = await Book.find({}).sort({ createdAt: -1 });
+    res.render("index", { books });
+  } catch (error) {
+    next(error);
+  }
 });
 
 
