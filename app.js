1b8dc65db0eff384886eb552c4748e5491893309
Abhishek Goswami <abhishekgoswami157@gmail.com>
2021-06-08 17:40:20 +0530

    Add env file

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var flash = require('connect-flash');
-var passport = require('passport');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
+var flash = require("connect-flash");
+var passport = require("passport");
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var booksRouter = require("./routes/books");
+var adminRouter = require("./routes/admin");
+var reviewsRouter = require("./routes/reviews");
+var cartsRouter = require("./routes/carts");
+var auth = require("./middlewares/auth");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var booksRouter = require('./routes/books');
-var adminRouter = require('./routes/admin');
-var reviewsRouter = require('./routes/reviews');
-var cartsRouter = require('./routes/carts');
-var auth = require('./middlewares/auth');
+require("dotenv").config();
+require("./modules/passport");
 
-require('dotenv').config();
-require('./modules/passport')
-
-
-mongoose.connect('mongodb://localhost/bookstore', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, err => console.log("Connected", err ? err : true));
+mongoose.connect(
+  process.env.MONGO_DB,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (err) => console.log("Connected", err ? err : true)
+);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -40,40 +42,40 @@ app.use(
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(flash());
 
 app.use(auth.userInfo);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/books', booksRouter);
-app.use('/admin', adminRouter);
-app.use('/reviews', reviewsRouter);
-app.use('/carts', cartsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+app.use("/admin", adminRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/carts", cartsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
-var path = require('path');
-var defaultImgPath = path.join(__dirname, "../", "public/images/bookshelf-svgrepo-com.svg")
+var bcrypt = require("bcrypt");
+var path = require("path");
+var defaultImgPath = path.join(
+  __dirname,
+  "../",
+  "public/images/bookshelf-svgrepo-com.svg"
+);
 
 var userSchema = new Schema(
   {
-    image: { type: String},
+    image: { type: String },
     name: { type: String },
     email: { type: String, required: true, unique: true },
     username: { type: String },
@@ -14,39 +18,40 @@ var userSchema = new Schema(
     github: {
       name: String,
       username: String,
-      image: String
+      image: String,
     },
     google: {
       name: String,
-      image: String
+      image: String,
     },
     providers: [String],
     isAdmin: { type: Boolean, default: false },
-    cartId: {type: Schema.Types.ObjectId, ref: "Cart"},
+    cartId: { type: Schema.Types.ObjectId, ref: "Cart" },
     favourites: [{ type: Schema.Types.ObjectId, ref: "Book" }],
-    isBlocked: { type: Boolean, default: false}
+    isBlocked: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
-
-userSchema.pre('save', function(next) {
-    if(this.password && this.isModified('password')) {
-        this.password = bcrypt.hashSync(this.password, 10);
-    }
-    next();
+userSchema.pre("save", function (next) {
+  if (this.password && this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, 10);
+  }
+  next();
 });
-userSchema.pre('save', function(next) {
-    if(this.email == "abhigoswami296@gmail.com" || this.email == "golugosu157@gmail.com") {
-        this.isAdmin = true;
-        
-    }
-    next();
+userSchema.pre("save", function (next) {
+  if (
+    this.email == "abhigoswami296@gmail.com" ||
+    this.email == "golugosu157@gmail.com" ||
+    this.email == "abhishek1@gmail.com"
+  ) {
+    this.isAdmin = true;
+  }
+  next();
+});
 
-})
-
-userSchema.methods.validatePassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
-}
+userSchema.methods.validatePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
